ac5757a00a245dcb02fa7bd1eb082e22423e521f
arik <arikhativa@gmail.com>
2019-04-05 14:59:30 +0200

    toup and to low no norm

---
+++ ft_tolower.c
@@ -0,0 +1,7 @@
+
+int ft_toupper(int n)
+{
+	if (n >= 'A' && n <= 'Z')
+		n = n + 32;
+	return (n);
+}
---
+++ ft_toupper.c
@@ -0,0 +1,7 @@
+
+int ft_toupper(int n)
+{
+	if (n >= 'a' && n <= 'z')
+		n = n - 32;
+	return (n);
+}
