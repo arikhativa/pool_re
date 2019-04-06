e39e67ca4d7b0384a3ebbf241ad2b29321e71725
arik <arikhativa@gmail.com>
2019-04-06 11:47:43 +0200

    g

--- ft_tolower.c
+++
@@ -1,7 +0,0 @@
-
-int ft_toupper(int n)
-{
-	if (n >= 'A' && n <= 'Z')
-		n = n + 32;
-	return (n);
-}
--- ft_toupper.c
+++
@@ -1,7 +0,0 @@
-
-int ft_toupper(int n)
-{
-	if (n >= 'a' && n <= 'z')
-		n = n - 32;
-	return (n);
-}
